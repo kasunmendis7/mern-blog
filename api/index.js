0b7bcc46457e40eabca1dd7ff631b247a43c53fa
Kasun Mendis <tkomendis@gmail.com>
2024-04-26 16:55:04 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,18 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+mongoose
+  .connect('mongodb+srv://kasun:kasun@mern-blog.ivr3glf.mongodb.net/mern-blog?retryWrites=true&w=majority&appName=mern-blog')
+  .then(() => {
+    console.log("MongoDb is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-
 app.listen(3000, () => {
-    console.log('Server is running on port 3000!');
+  console.log("Server is running on port 3000!");
 });
